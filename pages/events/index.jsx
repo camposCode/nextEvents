7f1f93083771466951d2af323514f844c14c2ed5
Jose Campos <camposplaza77@gmail.com>
2023-03-14 14:56:15 +0100

    browse all events done

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,6 +1,13 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from '../../components/events/event-list';
+
 const AllEventsPage = () => {
+    const events = getAllEvents();
+
     return (
-        <div>AllEventsPage</div>
+        <div>
+            <EventList items={ events }/>
+        </div>
     )
 }
 
